a312e32bb94a0ca029865335e9531ce5550b017d
Ngô Đức Dũng <ngoducdung@duck.com>
2023-10-28 09:46:31 +0000

    Update Optimizer.bat

--- Optimizer.bat
+++ Optimizer.bat
@@ -69,28 +69,6 @@ del /f /s /q "%appdata%\Spotify\Apps\Buddy-list.spa" >NUL 2>&1
 
 :: May add more in the future but idk
 
-::------
-
-echo ------------------------------------
-echo Task 1: All Temporary Files Deleted
-echo ------------------------------------
-timeout 1 > NUL
-echo Task 2: Compressing Windows...
-echo ------------------------------------
-timeout 1 > NUL
-
-Compact.exe /CompactOS:always > NUL
-echo Task 3: Spoofing HWID...
-echo ------------------------------------
-timeout 1 > NUL
-
-:: Additional Tweaks
-%WINDIR%\AntiOS\generate_fingerprint.py > NUL
-reg.exe add "HKLM\SOFTWARE\NVIDIA Corporation\Global" /v "{41FCC608-8496-4DEF-B43E-7D9BD675A6FF}" /t REG_BINARY /d "01" /f
-reg.exe add "HKLM\SYSTEM\ControlSet001\Services\nvlddmkm" /v "{41FCC608-8496-4DEF-B43E-7D9BD675A6FF}" /t REG_BINARY /d "01" /f
-"C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe" /Profile1 /Q
-cls
-
 echo ----------------------------------
 echo Done...Quitting
 echo ----------------------------------
